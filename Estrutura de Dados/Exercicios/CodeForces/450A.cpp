6c32d6e826a9e64d47dcb070f07ded0b3b3d730f
Erika Espindola <erikaaespindola@hotmail.com>
2018-03-16 23:23:46 -0300

    Update 450A.cpp

--- Estrutura de Dados/Exercicios/CodeForces/450A.cpp
+++ Estrutura de Dados/Exercicios/CodeForces/450A.cpp
@@ -1,1 +1,35 @@
+#include <bits/stdc++.h>
+#define MAX 110
 
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+
+	int n, m, vet[MAX];
+	queue<int> fila;
+
+	cin >> n >> m;
+
+	for(int i = 1; i <= n; i++) {
+		cin >> vet[i];
+		fila.push(i);
+	}
+
+	while(!fila.empty()) {
+		int a = fila.front();
+		fila.pop();
+
+		if(fila.empty()) {
+			cout << a << endl;
+			break;
+		}		
+
+		vet[a] -= m;
+
+		if(vet[a] > 0)
+			fila.push(a);
+	}	
+
+	return 0;
+}
