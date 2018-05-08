20ebc329cee2e82e7e64e51593dbdeea6e12153f
Erika Espindola <erikaaespindola@hotmail.com>
2018-05-08 16:16:41 +0000

    Create Fatorial.cpp

---
+++ Maratonas SBC/2015/Fatorial.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define N 8
+int fat[N] = {40320, 5040, 720, 120, 24, 6, 2, 1};
+
+int main(){
+	ios_base::sync_with_stdio(false);
+
+	int n, cont = 0;
+	
+	cin >> n;
+
+	while(n > 0){
+		for(int i = 0; i < N; i++){
+			if(fat[i] <= n){
+				n -= fat[i];
+				cont++;
+				break;
+			}
+		}
+	}
+
+	cout << cont << endl;
+	
+	return 0;
+}
